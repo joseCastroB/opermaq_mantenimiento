b4bc567150cdae35200b8b185eafd44a4b4ffcf4
joseCastroB <josecastrobarreto10@gmail.com>
2026-03-25 16:41:30 -0500

    se agrega sección de caracteristicas

--- models/maintenance_equipment.py
+++ models/maintenance_equipment.py
@@ -28,3 +28,16 @@ class MaintenaceEquipment(models.Model):
     medida_ruedas = fields.Char(string='Medida de Ruedas')
 
     alias = fields.Char(string='Alias')
+
+    # ==========================================
+    # SECCIÓN: CARACTERÍSTICAS TÉCNICAS
+    # ==========================================
+
+    fecha_compra = fields.Date(string='Fecha compra')
+    ano_fabricacion = fields.Integer(string='Año de fabricación')
+    temperatura_trabajo = fields.Char(string='Temperatura de trabajo')
+    peso = fields.Char(string='Peso')
+    componentes = fields.Char(string='Componentes')
+    tipo_rueda = fields.Char(string='Tipo de rueda')
+    altura_elevacion = fields.Char(string='Altura de elevación')
+    capacidad_carga = fields.Char(string='Capacidad de carga')
